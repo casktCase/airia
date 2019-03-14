0ce10e93dd0c0ccb04bab3cd06d1233b89fd7b5a
casktCase <caseyrudge@gmail.com>
2019-03-14 00:53:03 -0600

    Adding config file

---
+++ src/config.js
@@ -0,0 +1,10 @@
+//Store config settings in named object arrays for now.  Migrate if persistent storage is added
+
+//Network IOT devices
+export const netiotconfig = { device1IP: '192.168.1.181', device2IP: '192.168.1.182' };
+
+//Target URL for local weather JSON Feed
+export const weatherurl = { url: 'https://forecast.weather.gov/MapClick.php?lat=39.1431&lon=-104.4913&unit=0&lg=english&FcstType=json' };
+
+//network connected webcam IP addresses 
+export const cameraips = { camera1: '192.168.1.191', camera2: '192.168.1.192' };
