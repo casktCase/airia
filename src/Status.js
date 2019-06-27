e5763259b50a0516ee08398f7a3e175f18f33ada
casktcase <caseyrudge@gmail.com>
2019-06-26 23:38:00 -0600

    Adding logic to status allowing to display values from sensors

--- src/Status.js
+++ src/Status.js
@@ -1,22 +1,28 @@
-import React, { Component, useState, useEffect } from "react";
-
-
-
+import React, { useState, useEffect } from "react";
+import useAxios from 'axios-hooks';
 function Status() {
 
+    //Loading data served from express is at http://url:port/(dir)
+    //https://github.com/simoneb/axios-hooks
+    //Refetch timer is set for every 10 seconds
+    
+    const [{ data, loading, error }, refetch] = useAxios(
+        '/temp'
+      )
+
+      var timer = setInterval(()=>  refetch('/temp'), 10000)
+
+    //this stores a number in local session.  It indicates to the user 
+    //how many times a reset button was clicked
     const initialCount = () => Number(window.localStorage.getItem('count') || 0);
-
-
     const [count, setCount] = useState(initialCount);
+
     useEffect(
         () => {
             window.localStorage.setItem('count', count)
         },
         [count],
-
     )
-
-
 
     return (
         <div className="app">
@@ -25,7 +31,10 @@ function Status() {
                     Reset alarm
                 </button>
             </p>
-
+            <div>
+      <button id="btnRefetch" onClick={refetch}>refetch</button>
+      <pre>{ JSON.stringify(data, null, 2) }</pre>
+    </div>
         </div>
     );
 }
